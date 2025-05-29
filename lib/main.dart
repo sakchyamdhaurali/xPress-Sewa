25ab1d46ed4f75137dc6e2cddf683df8622ed786
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-29 15:51:23 +0545

    flutter sizer added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:xpress_sewa/views/splash_screen/splash_screen.dart';
 
 void main() {
@@ -6,23 +7,26 @@ void main() {
 }
 
 class XPressSewa extends StatelessWidget {
-const XPressSewa({ super.key });
+  const XPressSewa({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        colorSchemeSeed: Colors.white,
-      ),
-      initialRoute: "/",
-      routes: _appRoutes,
+  Widget build(BuildContext context) {
+    return FlutterSizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            useMaterial3: true,
+            brightness: Brightness.light,
+            colorSchemeSeed: Colors.white,
+          ),
+          initialRoute: "/",
+          routes: _appRoutes,
+        );
+      },
     );
   }
 }
-
 
 final Map<String, WidgetBuilder> _appRoutes = {
   "/": (context) => const SplashScreen(),
