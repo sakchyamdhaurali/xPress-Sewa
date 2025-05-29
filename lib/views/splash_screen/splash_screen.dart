73701354cf3165188cdd86109862efd4f9e50005
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-29 16:02:43 +0545

    mounted

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -5,20 +5,21 @@ import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:xpress_sewa/views/onboarding_screen/onboarding.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
+  const SplashScreen({ super.key });
 
   @override
-  _SplashScreenState createState() => _SplashScreenState();
+  SplashScreenState createState() => SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
     //Set time for splash screen display
-    Timer(Duration(seconds: 4), (){
+    Future.delayed(const Duration(seconds: 3), (){
+      if (!mounted) return;
       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const OnboardingScreen()));
     }
 
@@ -36,8 +37,8 @@ class _SplashScreenState extends State<SplashScreen> {
             children: [
               Center(
                 child: Container(
-                  height: 200.h,
-                  width: 200.w,
+                  height: 50.h,
+                  width: 50.w,
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: const Color(0xFF41626A),
