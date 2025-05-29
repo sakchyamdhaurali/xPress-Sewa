742a227ba89d32647b473dfbb7c53459f43ac0be
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-29 17:43:48 +0545

    onboarding controller

--- lib/views/onboarding_screen/onboarding.dart
+++ lib/views/onboarding_screen/onboarding.dart
@@ -1,23 +1,7 @@
 import 'package:flutter/material.dart';
-// import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:xpress_sewa/views/onboarding_screen/widgets.dart';
 
-// class OnboardingScreen extends StatelessWidget {
-// const OnboardingScreen({ super.key });
 
-//   @override
-//   Widget build(BuildContext context){
-//     return Container(
-//       color: const Color(0xFF41626A),
-//       child: SafeArea(
-//         child: Scaffold(
-//           backgroundColor: Colors.white,
-//           body: Center(child: Text("Onboarding")),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class OnboardingScreen extends StatelessWidget {
 const OnboardingScreen({ super.key });
@@ -33,9 +17,9 @@ const OnboardingScreen({ super.key });
           body: PageView(
             controller: PageController(),
             children: [
-              appOnBoardingPage(),
-              appOnBoardingPage(),
-              appOnBoardingPage(),
+              appOnBoardingPage(title: "Welcome to Xpress Sewa", subTitle: "Express Sewa is a fast, service-based app that quickly connects users to hire anything they need, anytime, anywhere."),
+              appOnBoardingPage(title: "Get Services", subTitle: "Take an Appointment for Super Fast Services"),
+              appOnBoardingPage(title: "Service Appointment", subTitle: "Take an Appointment for Your Work"),
         
             ],
           ),
