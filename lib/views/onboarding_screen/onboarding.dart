a2d1ab823418116d4d53d69ab6ccc31844589d1b
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-29 17:13:45 +0545

    page view added

--- lib/views/onboarding_screen/onboarding.dart
+++ lib/views/onboarding_screen/onboarding.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
+// import 'package:flutter_sizer/flutter_sizer.dart';
+import 'package:xpress_sewa/views/onboarding_screen/widgets.dart';
+
+// class OnboardingScreen extends StatelessWidget {
+// const OnboardingScreen({ super.key });
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Container(
+//       color: const Color(0xFF41626A),
+//       child: SafeArea(
+//         child: Scaffold(
+//           backgroundColor: Colors.white,
+//           body: Center(child: Text("Onboarding")),
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 class OnboardingScreen extends StatelessWidget {
 const OnboardingScreen({ super.key });
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+
     return Container(
       color: const Color(0xFF41626A),
       child: SafeArea(
         child: Scaffold(
           backgroundColor: Colors.white,
-          body: Center(child: Text("Onboarding")),
+          body: PageView(
+            controller: PageController(),
+            children: [
+              appOnBoardingPage(),
+              appOnBoardingPage(),
+              appOnBoardingPage(),
+        
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
