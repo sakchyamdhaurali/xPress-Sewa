8fcc62c3aa1689cfbed02ac29dc639cf2b39f405
dhaurali <sakchyamdhaurali5@gmail.com>
2025-05-29 18:23:47 +0545

    width height modified

--- lib/views/splash_screen/splash_screen.dart
+++ lib/views/splash_screen/splash_screen.dart
@@ -1,11 +1,10 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_sizer/flutter_sizer.dart';
 import 'package:xpress_sewa/views/onboarding_screen/onboarding.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({ super.key });
+  const SplashScreen({super.key});
 
   @override
   SplashScreenState createState() => SplashScreenState();
@@ -18,13 +17,15 @@ class SplashScreenState extends State<SplashScreen> {
     super.initState();
 
     //Set time for splash screen display
-    Future.delayed(const Duration(seconds: 3), (){
+    Future.delayed(const Duration(seconds: 3), () {
       if (!mounted) return;
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=> const OnboardingScreen()));
-    }
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const OnboardingScreen()),
+      );
+    });
+  }
 
-    );
-    }
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -37,22 +38,19 @@ class SplashScreenState extends State<SplashScreen> {
             children: [
               Center(
                 child: Container(
-                  height: 50.h,
-                  width: 50.w,
+               height: MediaQuery.of(context).size.height * 0.5,
+                width: MediaQuery.of(context).size.width * 0.5,
+
                   decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: const Color(0xFF41626A),
-                      
                   ),
                 ),
               ),
-          
-              Center(child: Image.asset("assets/branding.png"))
-          
-            
+
+              Center(child: Image.asset("assets/branding.png")),
             ],
           ),
-          
         ),
       ),
     );
